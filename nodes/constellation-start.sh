368919d0cf8adff61e058f7a330167fb2aa6d90d
Charles Hsiao <skywoods917@gmail.com>
2019-10-30 23:15:25 +0800

    Constellation single node

--- nodes/constellation-start.sh
+++ nodes/constellation-start.sh
@@ -9,27 +9,23 @@ fi
 
 echo "[*] Starting $numNodes Constellation node(s)"
 
-for i in `seq 1 ${numNodes}`
-do
-    DDIR="qdata/c$i"
-    mkdir -p $DDIR
-    mkdir -p qdata/logs
-    cp "keys/tm$i.pub" "$DDIR/tm.pub"
-    cp "keys/tm$i.key" "$DDIR/tm.key"
-    rm -f "$DDIR/tm.ipc"
-    CMD="constellation-node --url=https://127.0.0.$i:900$i/ --port=900$i --workdir=$DDIR --socket=tm.ipc --publickeys=tm.pub --privatekeys=tm.key --othernodes=https://127.0.0.1:9001/"
-    echo "$CMD >> qdata/logs/constellation$i.log 2>&1 &"
-    $CMD >> "qdata/logs/constellation$i.log" 2>&1 &
-done
+INDEX_NODE=$(cat ~/node_config | grep "NODE_INDEX" | awk -F '=' '{print $2}')
+
+DDIR="qdata/c$INDEX_NODE"
+mkdir -p $DDIR
+mkdir -p qdata/logs
+cp "keys/tm$INDEX_NODE.pub" "$DDIR/tm.pub"
+cp "keys/tm$INDEX_NODE.key" "$DDIR/tm.key"
+rm -f "$DDIR/tm.ipc"
+CMD="constellation-node --url=https://127.0.0.$INDEX_NODE:900$INDEX_NODE/ --port=900$INDEX_NODE --workdir=$DDIR --socket=tm.ipc --publickeys=tm.pub --privatekeys=tm.key --othernodes=https://127.0.0.1:9001/"
+echo "$CMD >> qdata/logs/constellation$INDEX_NODE.log 2>&1 &"
+nohup $CMD >> "qdata/logs/constellation$INDEX_NODE.log" 2>&1 &
 
 DOWN=true
 while $DOWN; do
     sleep 0.1
     DOWN=false
-    for i in `seq 1 ${numNodes}`
-    do
-	if [ ! -S "qdata/c$i/tm.ipc" ]; then
-            DOWN=true
-	fi
-    done
+    if [ ! -S "qdata/c$INDEX_NODE/tm.ipc" ]; then
+      DOWN=true
+    fi
 done
