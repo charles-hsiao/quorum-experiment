8283f3233fd25d2bf0e0d478d6ca4b0f996f9a72
Charles Hsiao <skywoods917@gmail.com>
2019-12-26 23:44:17 +0800

    Update event loop count

--- nodes/samples/simple-event/public-generate-events.js
+++ nodes/samples/simple-event/public-generate-events.js
@@ -10,6 +10,6 @@ var address="0x1932c48b2bf8102ba33b4a6b545c32236e342f34"
 var abi = [{"constant":true,"inputs":[],"name":"storedData","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"x","type":"uint256"}],"name":"set","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"get","outputs":[{"name":"retVal","type":"uint256"}],"payable":false,"type":"function"},{"inputs":[{"name":"initVal","type":"uint256"}],"payable":false,"type":"constructor"}];
 var contract = eth.contract(abi).at(address)
 
-for ( i = 0; i < 20; i++) {
+for ( i = 0; i < 1000; i++) {
   contract.set(4,{from:eth.accounts[0]});
 }
