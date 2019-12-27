0e425763e7f86600904a4740670eb63dcc57ae61
Charles Hsiao <skywoods917@gmail.com>
2019-12-27 20:11:33 +0800

    Remove times loop

--- nodes/samples/simple-event/private-generate-events-50.js
+++ nodes/samples/simple-event/private-generate-events-50.js
@@ -1,27 +1,12 @@
-// From console test node 1 & 7 to see if the events were recorded
-// seth is a cool CLI (but you can use whatever you'd like): https://github.com/dapphub/dapptools
+// From the console test nodes 1 & 7 to see if the events were recorded
+// seth is a cool cli tool that can be used see: https://github.com/dapphub/dapptools
 //   $> seth events 0x1932c48b2bf8102ba33b4a6b545c32236e342f34 | wc -l
-
-// NOTE: replace this with the address of the contract you wish to generate
-//       events for.
 var address="0x1932c48b2bf8102ba33b4a6b545c32236e342f34"
 
 // simple contract
 var abi = [{"constant":true,"inputs":[],"name":"storedData","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"x","type":"uint256"}],"name":"set","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"get","outputs":[{"name":"retVal","type":"uint256"}],"payable":false,"type":"function"},{"inputs":[{"name":"initVal","type":"uint256"}],"payable":false,"type":"constructor"}];
 var contract = eth.contract(abi).at(address)
 
-for ( i = 0; i < 100; i++) {
-  for ( i = 0; i < 50; i++) {
-    contract.set(4,{from:eth.accounts[0]});
-  }
-  sleep(1000)
-}
-
-function sleep(milliseconds) {
-  var start = new Date().getTime();
-  for (var i = 0; i < 1e7; i++) {
-    if ((new Date().getTime() - start) > milliseconds){
-      break;
-    }
-  }
+for ( i = 0; i < 50; i++) {
+  contract.set(4,{from:eth.accounts[0], privateFor:["1iTZde/ndBHvzhcl7V68x44Vx7pl8nwx9LqnM/AfJUg="]});
 }
