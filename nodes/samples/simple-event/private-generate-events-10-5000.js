19941ec5da86ca376aa1955454bb1d8c278a030f
Charles Hsiao <skywoods917@gmail.com>
2019-12-27 16:37:32 +0800

    Fix incorrect number

--- nodes/samples/simple-event/private-generate-events-10-5000.js
+++ nodes/samples/simple-event/private-generate-events-10-5000.js
@@ -7,8 +7,8 @@ var address="0x1932c48b2bf8102ba33b4a6b545c32236e342f34"
 var abi = [{"constant":true,"inputs":[],"name":"storedData","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"x","type":"uint256"}],"name":"set","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"get","outputs":[{"name":"retVal","type":"uint256"}],"payable":false,"type":"function"},{"inputs":[{"name":"initVal","type":"uint256"}],"payable":false,"type":"constructor"}];
 var contract = eth.contract(abi).at(address)
 
-for ( i = 0; i < 50; i++) {
-  for ( i = 0; i < 100; i++) {
+for ( i = 0; i < 500; i++) {
+  for ( i = 0; i < 10; i++) {
     contract.set(4,{from:eth.accounts[0], privateFor:["1iTZde/ndBHvzhcl7V68x44Vx7pl8nwx9LqnM/AfJUg="]});
   }
   sleep(1000)
